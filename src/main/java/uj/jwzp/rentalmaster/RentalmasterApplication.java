da9965f57ec004b31e84ec8b0ab1de7eea5f5f7c
Marek Ciążyński <m.ciazynski@protonmail.com>
2023-03-16 11:05:19 +0100

    Initial commit
    Project generated with Sprint Initializr

---
+++ src/main/java/uj/jwzp/rentalmaster/RentalmasterApplication.java
@@ -0,0 +1,13 @@
+package uj.jwzp.rentalmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uj/jwzp/rentalmaster/RentalmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package uj.jwzp.rentalmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
